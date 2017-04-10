45fb46f4ec513ce91e49cb1035e85cc0cdd1d39e
Collaborne <info@collaborne.com>
2017-04-10 12:58:27 +0000

    Deploy to GitHub Pages

---
+++ components/carbon-i18n-behavior/demo/i18n/de.js
@@ -0,0 +1,7 @@
+!function() {
+    Polymer.CarbonI18nBehaviorLocales.add('simple-i18n', 'de', {
+        hello: 'Hallo Welt',
+        members: '{0} Mitglieder',
+        intro: 'Mein Name ist {0} und ich bin {1} Jahre alt.'
+    });
+}();
---
+++ components/carbon-i18n-behavior/demo/i18n/en.js
@@ -0,0 +1,7 @@
+!function() {
+    Polymer.CarbonI18nBehaviorLocales.add('simple-i18n', 'en', {
+        hello: 'Hello World',
+        members: '{0} members',
+        intro: 'My name is {0} and I am {1} years old.'
+    });
+}();
